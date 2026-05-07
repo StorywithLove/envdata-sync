4e02a6583117e32f5dc1676de40221648853af05
StorywithLove <48684315+StorywithLove@users.noreply.github.com>
2026-05-07 18:11:07 +0800

    Update zjemc_playwright.py

--- zjemc_playwright.py
+++ zjemc_playwright.py
@@ -59,7 +59,7 @@ def get_decrypted_aqi_data():
         df = df.drop(['evatime'], axis=1)
 
         timestamp = df.time.iloc[0].strftime(format="%Y-%m-%dT%H")
-        daily_folder = Path('Archive')/timestamp[:10]
+        daily_folder = Path('Archive')/'ZJEMC'/timestamp[:10]
         daily_folder.mkdir(parents=True, exist_ok=True)
         df.to_csv(daily_folder/(timestamp+'.csv'), mode='w')
         
